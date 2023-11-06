d91a61cde105efbf0b7ef5cf82edb833644494b2
tsrsahackathon <149416052+tsrsahackathon@users.noreply.github.com>
2023-11-06 20:50:31 +0530

    Update record.py

--- record.py
+++ record.py
@@ -1,49 +1,49 @@
 import pyaudio
 import wave
-import RPi.GPIO as GPIO # import GPIO library to use gpio pins
+import RPi.GPIO as GPIO
 
-form_1 = pyaudio.paInt16 # 16-bit resolution
-chans = 1 #1 channel
-samp_rate = 16000# 16kHz sampling rate
-chunk = 4096 # 2^12 samples for buffer
-dev_index = 2 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'test1.wav' # name of .wav file
+form_1 = pyaudio.paInt16  # 16-bit resolution
+chans = 1  # 1 channel
+samp_rate = 16000  # 44.1kHz sampling rate
+chunk = 4096  # 2^12 samples for buffer
+dev_index = 2  # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav'  # name of .wav file
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
+audio = pyaudio.PyAudio()
 
 # create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                    input_device_index = dev_index,input = True, \
+stream = audio.open(format=form_1, rate=samp_rate, channels=chans,
+                    input_device_index=dev_index, input=True,
                     frames_per_buffer=chunk)
 
-# set up gpio pin 17 as input with pull-down resistor
-GPIO.setmode(GPIO.BCM) # use BCM numbering scheme
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set pin 17 as input with pull-down
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-print("press the button to start recording")
+print("Waiting for button press...")
+
+while GPIO.input(3) == GPIO.LOW:
+    pass
+while GPIO.input(3) == GPIO.HIGH:
+    pass
+
+print("Button pressed, recording...")
+
 frames = []
 
-# loop until the button is pressed twice
-while True:
-    # wait for the button to be pressed
-    GPIO.wait_for_edge(17, GPIO.RISING)
-    print("recording")
-    # record audio until the button is pressed again
-    while not GPIO.input(17):
-        data = stream.read(chunk)
-        frames.append(data)
-    print("finished recording")
-    # break the loop if the button is pressed twice
-    if len(frames) > 0:
-        break
+while GPIO.input(3) == GPIO.LOW:
+    data = stream.read(chunk)
+    frames.append(data)
 
-# stop the stream, close it, and terminate the pyaudio instantiation
+print("Button released, stopped recording.")
+print("Finished recording.")
+
+# Stop the stream, close it, and terminate the pyaudio instantiation
 stream.stop_stream()
 stream.close()
 audio.terminate()
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
+# Save the audio frames as a .wav file
+wavefile = wave.open(wav_output_filename, 'wb')
 wavefile.setnchannels(chans)
 wavefile.setsampwidth(audio.get_sample_size(form_1))
 wavefile.setframerate(samp_rate)
